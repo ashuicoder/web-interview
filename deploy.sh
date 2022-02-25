3ce21edf161d2a7596a50351b50c900a686df717
shaozongyao <1249735120@qq.com>
2022-02-25 16:33:04 +0800

    fix

--- deploy.sh
+++ deploy.sh
@@ -21,6 +21,6 @@ git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # /**这个地方换成自己的github地址和仓库地址**/
-git push -f git@github.com:ashuicoder/web-interview.git main:gh-pages
+git push -f git@github.com:ashuicoder/web-interview.git master:gh-pages
 
 cd -
